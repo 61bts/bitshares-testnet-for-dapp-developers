bbc0d8ebdc5358e59dff31bb5e14bb74b0284acf
ETY001 <ety001@domyself.me>
2019-12-06 11:34:40 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="md-layout app">
-    <div class="md-layout-item">
-      <h1>Bitshares Testnet For Dapp Developers</h1>
-      <md-tabs class="md-primary" md-alignment="centered">
-        <md-tab id="intro" :md-label="$t('intro')">
-          <Intro></Intro>
-        </md-tab>
-        <md-tab id="accounts" md-label="创建账号"></md-tab>
-      </md-tabs>
+  <div class="app">
+    <div class="md-layout">
+      <div class="md-layout-item">
+        <h1>Bitshares Testnet For Dapp Developers</h1>
+        <md-tabs class="md-primary" md-alignment="centered">
+          <md-tab id="intro" :md-label="$t('intro')">
+            <Intro></Intro>
+          </md-tab>
+          <md-tab id="accounts" md-label="创建账号"></md-tab>
+        </md-tabs>
+      </div>
+    </div>
+    <div class="md-layout">
+      <div class="md-layout-item copyright">
+        &copy; ETY001
+      </div>
     </div>
   </div>
 </template>
@@ -29,6 +36,10 @@ export default {
     width: 80%;
   }
   h1 {
+    text-align: center;
+  }
+  .copyright {
+    margin-top: 20px;
     text-align: center;
   }
 </style>
