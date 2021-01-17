1166347d5d45a86618b3673059f6b68bc43ee250
ety001 <ety001@users.noreply.github.com>
2021-01-17 12:35:19 +0800

    Update Intro.vue

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -3,6 +3,7 @@
     <p>{{ $t('intro_desp') }}</p>
     <p>{{ $t('intro_desp1') }}</p>
     <p>{{ $t('api_url') }} : <pre>wss://api-testnet-private.61bts.com</pre></p>
+    <p>{{ $t('chain_id') }} : <pre>2d20869f3d925cdeb57da14dec65bbc18261f38db0ac2197327fc3414585b0c5</pre></p>
   </div>
 </template>
 
